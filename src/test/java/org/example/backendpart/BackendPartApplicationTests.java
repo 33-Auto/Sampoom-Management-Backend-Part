f167ba2575600c7345770f17755e9507e2d292a3
yangjiseonn <yangjiseonn@gmail.com>
2025-10-08 14:59:06 +0900

    initial commit

---
+++ src/test/java/org/example/backendpart/BackendPartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendpart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
