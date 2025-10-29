1a5ed25c1cf9adb78ef0d7fdde744b9f35756864
Kim Taemin <ruhokim1@naver.com>
2025-10-29 22:28:31 +0900

    [FIX] JacksonConfig 수정

--- src/main/java/com/sampoom/backend/common/config/JacksonConfig.java
+++ src/main/java/com/sampoom/backend/common/config/JacksonConfig.java
@@ -3,6 +3,7 @@ package com.sampoom.backend.common.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +15,13 @@ public class JacksonConfig {
         ObjectMapper mapper = new ObjectMapper();
         // Hibernate Lazy 프록시 무시
         mapper.registerModule(new Hibernate6Module());
+        // Java 8 시간 API 지원 (LocalDateTime 등)
+        mapper.registerModule(new JavaTimeModule());
         // 비어 있는 Bean 직렬화 오류 방지
         mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        // LocalDateTime을 타임스탬프가 아닌 문자열로 직렬화
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         return mapper;
     }
 }
